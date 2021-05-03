ba2ebab2e6cc887b251c8772030da459105bd1e2
diamkil <diamkill@gmail.com>
2021-05-03 07:52:31 -0400

    Update Docker-Start.sh

--- Docker-Start.sh
+++ Docker-Start.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-mkdir -p /data/notea
+if [ -d "/data/notea" ]
+then
+    echo "Notea Bucket already exists, continuing" 
+else
+    echo "Notea Bucket doesn't exist, creating it"
+    mkdir -p /data/notea
+fi
 
+echo "Starting MinIO"
 screen -d -m ./MinIO-Start.sh
 
-screen -d -m ./Notea-Start.sh
-
-/bin/sh
+echo "Starting Notea"
+./Notea-Start.sh
