63e0176f19e10345e56be7cfcd549f5ff620f8b6
diamkil <diamkill@gmail.com>
2021-05-02 22:12:54 -0400

    Update Notea-Start.sh

--- Notea-Start.sh
+++ Notea-Start.sh
@@ -7,5 +7,6 @@ export STORE_SECRET_KEY=$STORE_PASSWORD
 export STORE_BUCKET=notea
 export STORE_END_POINT=http://localhost:9000
 export STORE_FORCE_PATH_STYLE=true
+export COOKIE_SECURE=false
 
 /notea/node_modules/.bin/next start
