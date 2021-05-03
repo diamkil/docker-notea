d2564f6c198389acfde68b535395fb64c6192636
diamkil <35732047+diamkil@users.noreply.github.com>
2021-05-03 11:49:01 -0400

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-time=$(date +%s)
 docker pull cinwell/notea:latest
 docker pull minio/minio:latest
 
-docker build . -t diamkil/notea:latest -t diamkil/notea:$time
+docker build . -t diamkil/notea:latest
 
 echo "Would you like to push? (y/n)"
 read -n 1 confirm
@@ -13,7 +12,6 @@ if [ "$confirm" == "y" ];
 then
     echo "Pushing"
     docker push diamkil/notea:latest
-    docker push diamkil/notea:$time
 else
     echo "Not Pushing"
 fi
