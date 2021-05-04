dd93e064961cb0a067c51544617aced8f485100b
diamkil <35732047+diamkil@users.noreply.github.com>
2021-05-04 09:30:46 -0400

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -4,14 +4,4 @@ docker pull minio/minio:latest
 
 docker build . -t diamkil/notea:latest
 
-echo "Would you like to push? (y/n)"
-read -n 1 confirm
-echo ""
-
-if [ "$confirm" == "y" ];
-then
-    echo "Pushing"
-    docker push diamkil/notea:latest
-else
-    echo "Not Pushing"
-fi
+docker push diamkil/notea:latest
