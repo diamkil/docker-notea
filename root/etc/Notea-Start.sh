34e6118a9922fa564c0824cf87567c31d596c55c
diamkil <diamkill@gmail.com>
2021-05-08 14:52:25 -0400

    Update Notea-Start.sh

--- root/etc/Notea-Start.sh
+++ root/etc/Notea-Start.sh
@@ -6,5 +6,6 @@ export STORE_BUCKET=notea
 export STORE_END_POINT=$STORE_URL
 export STORE_FORCE_PATH_STYLE=true
 export COOKIE_SECURE=false
+export DIRECT_RESPONSE_ATTACHMENT=true
 
 /notea/node_modules/.bin/next start
